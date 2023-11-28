a09509250da977fb0a1bce5b171b22f2a380a73c
kharchenkok <k.v.kharchenko@ukr.net>
2023-11-28 17:57:54 +0200

    hooks added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Section from './Section';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 
-class App extends Component {
-  state = {
+function App() {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleClick = event => {
+  const handleClick = event => {
     const { name } = event.target;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [name]: prevFeedback[name] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    const feedbackValues = Object.values(this.state);
-    return feedbackValues.reduce((total, value) => total + value, 0);
-  };
+  const totalFeedback = Object.values(feedback).reduce(
+    (total, value) => total + value,
+    0
+  );
+  const positivePercentage =
+    Math.round((feedback.good / totalFeedback) * 100) || 0;
+  const options = Object.keys(feedback);
+  const feedbackData = Object.entries(feedback).map(([label, value]) => ({
+    label,
+    value,
+  }));
 
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedback = this.countTotalFeedback();
-    const { good } = this.state;
-
-    return Math.round((good / totalFeedback) * 100) || 0;
-  };
-
-  getFeedbackData = () =>
-    Object.entries(this.state).map(([label, value]) => ({
-      label,
-      value,
-    }));
-
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
-    const feedbackData = this.getFeedbackData();
-
-    const statisticsList = Object.entries(this.state);
-    return (
-      <div className="container">
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleClick}
+  return (
+    <div className="container">
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+      </Section>
+      {totalFeedback > 0 ? (
+        <Section title={'Statistics'}>
+          <Statistics
+            list={feedbackData}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
           />
         </Section>
-        {totalFeedback > 0 ? (
-          <Section title={'Statistics'}>
-            <Statistics
-              list={feedbackData}
-              total={totalFeedback}
-              positivePercentage={positivePercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message={'There is no feedback'} />
-        )}
-      </div>
-    );
-  }
+      ) : (
+        <Notification message={'There is no feedback'} />
+      )}
+    </div>
+  );
 }
 
 export default App;
